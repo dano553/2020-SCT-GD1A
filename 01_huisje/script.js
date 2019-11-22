8d93b380646301119c2923bc81bc83db80696c12
dano553 <14122002+dano553@users.noreply.github.com>
2019-11-22 15:08:52 +0100

    test

--- 01_huisje/script.js
+++ 01_huisje/script.js
@@ -11,3 +11,42 @@ canvas.width = width;
 canvas.height = height;
 
 // hier kun je op de canvas tekenen
+context.beginPath();
+context.lineWidth = 20;
+context.fillStyle = "red";
+context.moveTo(300,100);
+context.lineTo(200,300);
+context.lineTo(600,400);
+context.lineTo(700,200);
+context.closePath();
+context.stroke();
+context.fill();
+
+
+context.beginPath();
+context.fillStyle = "gray";
+context.lineTo(200,300);
+context.lineTo(200,500);
+context.lineTo(600,600);
+context.lineTo(600,400);
+context.closePath();
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.fillStyle = "gray";
+context.lineTo(600,400);
+context.lineTo(600,600);
+context.lineTo(800,500);
+context.lineTo(800,300);
+context.lineTo(700,200);
+context.closePath();
+context.stroke();
+context.fill();
+
+
+context.beginPath();
+context.lineWidth = 15;
+context.lineTo(600,400);
+context.lineTo(800,300);
+context.stroke();
